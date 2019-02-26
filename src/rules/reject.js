c5fedffa6f983b24cba85e3b0c0f8dbfb2da5891
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-02-26 13:21:21 +0100

    fonctional rule

--- src/rules/reject.js
+++ src/rules/reject.js
@@ -7,47 +7,73 @@ module.exports = {
   meta: {
     type: 'suggestion',
     docs: {
-      url: docsUrl('reject'), //TODO: add documentation
+      url: docsUrl('reject'),
     },
     fixable: 'code',
   },
 
   create: function (context) {
+    const sourceCode = context.getSourceCode()
 
     return {
       'Literal[regex]': node => {
         const oldPattern = node.regex.pattern
         // const { flags } = node.regex
 
-        //const newPattern = cleanRegexp(oldPattern, flags)
         const parsed = parse(oldPattern)
-        console.log(parsed.alternatives[0].terms[0])
-        const ranges = parsed.alternatives[0].terms[0].atom.ranges || []
+        const alternative = parsed.alternatives[0] || []
+        const ranges = alternative.terms[0].atom.ranges || []
+
+        const isNegated = parsed.alternatives[0].terms[0].atom.negated || false
+
         const map1 = ranges.map(function(item) {
           // https://fr.wikipedia.org/wiki/American_Standard_Code_for_Information_Interchange
             return (item.end === item.start) && item.start <= 122 && item.start >= 97
         })
         const map2 = ranges.map(function(item, index) {
             const next = ranges[index + 1] || {start: -10}
-            const isLatest = index == (ranges.length -1)
-            const notA = item.start != 97
-            return (item.end + 1) == next.start || (isLatest && notA)
+            const isLatest = index === (ranges.length -1)
+            const notA = item.start !== 97
+            return (item.end + 1) === next.start || (isLatest && notA)
+        })
+        const map3 = ranges.map(function(item, index) {
+            const next = ranges[index + 1] || {start: -10}
+            const isLatest = index === (ranges.length -1)
+            const notZ = item.start !== 122
+            return (item.end - 1) === next.start || (isLatest && notZ)
         })
 
         // Handle regex literal inside RegExp constructor in the other handler
         //if (node.parent.type === 'NewExpression' && node.parent.callee.name === 'RegExp') {
         //  return
         //}
+        const needReport = (!map2.includes(false) || !map3.includes(false))
 
-        if (ranges.length > 1 && !map1.includes(false) && !map2.includes(false)) {
-          console.log("before report")
+        if (ranges.length > 1 && !map1.includes(false) && needReport) {
           context.report({
             node,
             message,
-            fix: fixer => fixer,
+            fix: function(fixer) {
+              const range = sourceCode.getTokenBefore(node).range
+              const extract = function (str) {
+                var nb = oldPattern.length - ranges.length
+
+                const firstLetter = String.fromCharCode(ranges[0].start)
+                // const lastLetter  = String.fromCharCode(ranges[ranges.length - 1].start)
+                if (isNegated) {
+                  const s = isNegated ? '^':''
+                  return '['+s+ firstLetter+'-'+ str.substr(-nb+1, nb+1)
+                }
+                return '['+firstLetter+'-'+ str.substr(-nb, nb)
+              }
+
+              //TODO: issue ' '
+              return [
+                fixer.remove(node),
+                fixer.insertTextAfterRange(range, ' /'+extract(oldPattern)+'/'),
+              ]
+            },
           })
-          //console.error("llldld")
-          console.log("after report")
         }
       },
     }
--- tests/src/core/rules/reject.js
+++ tests/src/core/rules/reject.js
@@ -11,7 +11,10 @@ ruleTester.run('reject', rule, {
     test({code: 'const simple = /ab/;'}),
     test({code: 'const simple = /adjc/;'}),
     test({code: 'const simple = /[ad]/;'}),
-    test({code: 'const simple = /[a]/;'}),
+    test({code: 'const simple = /[`a]/;'}),
+    test({code: 'const simple = /[z{]/;'}),
+    test({code: 'const simple = /[a][b]/;'}),
+    test({code: 'const simple = /[a]+[b]/;'}),
   ],
 
   invalid: [
@@ -21,6 +24,42 @@ ruleTester.run('reject', rule, {
         ruleId: 'Reject',
         message: 'message',
       }],
+      output: 'const simple = /[a-c]/ ;',
+    }),
+    test({
+      code: `const simple = /[abc]{1,2}/;`,
+      errors: [{
+        ruleId: 'Reject',
+        message: 'message',
+      }],
+      output: 'const simple = /[a-c]{1,2}/ ;',
+    }),
+    test({
+      code: `const simple = /[^abc]/;`,
+      errors: [{
+        ruleId: 'Reject',
+        message: 'message',
+      }],
+      output: 'const simple = /[^a-c]/ ;',
+    }),
+    test({
+      code: `const simple = /[^abc]+/;`,
+      errors: [{
+        ruleId: 'Reject',
+        message: 'message',
+      }],
+      output: 'const simple = /[^a-c]+/ ;',
+    }),
+    test({
+      code: `const simple = /[cba]/;`,
+      errors: [{
+        ruleId: 'Reject',
+        message: 'message',
+      }],
+    }),
+    test({
+      code: `const simple = /[a]/;`,
+      errors: 0,
     }),
   ],
 })
